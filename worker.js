b4242eb9df4ca9130a430440dd99798a1f036fa2
marceloprestes-hub <marceloprestes.economista@gmail.com>
2026-07-20 16:07:14 -0300

    Update worker.js

--- worker.js
+++ worker.js
@@ -1,11 +1,25 @@
 import { processarRotas } from "./src/rotas.js";
 
 export default {
+  async fetch(request, env, ctx) {
+    try {
+      return await processarRotas(request, env, ctx);
+    } catch (error) {
+      console.error("Erro MGR API:", error);
 
-  async fetch(request, env) {
-
-    return await processarRotas(request, env);
-
+      return new Response(
+        JSON.stringify({
+          sucesso: false,
+          mensagem: "Erro interno da API.",
+          erro: error.message
+        }),
+        {
+          status: 500,
+          headers: {
+            "Content-Type": "application/json;charset=UTF-8"
+          }
+        }
+      );
+    }
   }
-
 };
