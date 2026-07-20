a34b841f4487398a7e5ff13faa1364324defc20a
marceloprestes-hub <marceloprestes.economista@gmail.com>
2026-07-20 11:50:56 -0300

    Update banco.js

--- src/banco.js
+++ src/banco.js
@@ -1,11 +1,48 @@
 export async function listarLeads(env) {
-
     const resultado = await env.DB.prepare(
-
         "SELECT * FROM leads ORDER BY criado_em DESC"
-
     ).all();
 
     return resultado.results;
+}
 
+export async function buscarLead(id, env) {
+    return await env.DB.prepare(
+        "SELECT * FROM leads WHERE id=?"
+    ).bind(id).first();
+}
+
+export async function cadastrarLead(dados, env) {
+    return await env.DB.prepare(
+        `INSERT INTO leads
+        (nome,email,telefone,empresa,origem)
+        VALUES (?,?,?,?,?)`
+    ).bind(
+        dados.nome,
+        dados.email,
+        dados.telefone,
+        dados.empresa,
+        dados.origem
+    ).run();
+}
+
+export async function atualizarLead(id,dados,env){
+    return await env.DB.prepare(
+        `UPDATE leads
+        SET nome=?,email=?,telefone=?,empresa=?,origem=?
+        WHERE id=?`
+    ).bind(
+        dados.nome,
+        dados.email,
+        dados.telefone,
+        dados.empresa,
+        dados.origem,
+        id
+    ).run();
+}
+
+export async function excluirLead(id,env){
+    return await env.DB.prepare(
+        "DELETE FROM leads WHERE id=?"
+    ).bind(id).run();
 }
