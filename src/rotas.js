c63f6dd7b5333ae5003a38c11801d17eb241d8d4
marceloprestes-hub <marceloprestes.economista@gmail.com>
2026-07-20 12:07:41 -0300

    Update rotas.js

--- src/rotas.js
+++ src/rotas.js
@@ -1,44 +1,41 @@
 import { sucesso, erro } from "./retorno.js";
 
+import {
+    listarLeads,
+    buscarLead,
+    cadastrarLead,
+    atualizarLead,
+    excluirLead
+} from "./banco.js";
+
 export async function processarRotas(request, env) {
 
     const url = new URL(request.url);
-
     const rota = url.pathname;
-
     const metodo = request.method;
 
     if (rota === "/saude") {
-
         return sucesso({
-
             sistema: "MGR API",
-
             versao: "1.0.0",
-
             status: "ONLINE"
-
         });
-
     }
 
     if (rota === "/leads" && metodo === "GET") {
-
-        return sucesso({
-
-            mensagem: "Listagem de Leads"
-
-        });
-
+        const leads = await listarLeads(env);
+        return sucesso(leads);
     }
 
-    if (rota === "/leads" && metodo === "POST") {
+    if (rota === "/lead" && metodo === "POST") {
+
+        const dados = await request.json();
+
+        await cadastrarLead(dados, env);
 
         return sucesso({
-
-            mensagem: "Cadastro de Lead"
-
-        });
+            mensagem: "Lead cadastrado com sucesso."
+        },201);
 
     }
 
