523f8f67e79e191d72cb8b70d14e26c03d8ed1c8
marceloprestes-hub <marceloprestes.economista@gmail.com>
2026-07-20 11:39:05 -0300

    Update worker.js

--- worker.js
+++ worker.js
@@ -1,34 +1,11 @@
 import { processarRotas } from "./src/rotas.js";
 
 export default {
+
   async fetch(request, env) {
-    try {
-      const resposta = await processarRotas(request, env);
 
-      return new Response(
-        JSON.stringify(resposta, null, 2),
-        {
-          headers: {
-            "Content-Type": "application/json"
-          }
-        }
-      );
+    return await processarRotas(request, env);
 
-    } catch (erro) {
-
-      return new Response(
-        JSON.stringify({
-          sucesso: false,
-          erro: erro.message
-        }),
-        {
-          status: 500,
-          headers: {
-            "Content-Type": "application/json"
-          }
-        }
-      );
-
-    }
   }
+
 };
